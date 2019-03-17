7271a1c630fbfe44de7ed54b16015d86e4207327
Shaposhev Dmitriy <dimash@playrix.com>
2019-03-17 23:45:26 +0300

    Обновил иконку и изменил условие окна Rate Us

--- game.lua
+++ game.lua
@@ -568,7 +568,7 @@ local function continueGame()
 	composer.setVariable( "finalScore", score )
 	adCounter = adCounter+1
 	rateUsCounter = rateUsCounter+1
-	if rateUsCounter == 20 then
+	if rateUsCounter == 8 and lives == 3 then
 		-- Проверяем, ставил ли игрок оценку/отзыв
 		local file_rateUs, errorString = io.open( filePathRateUs, "rb" )
 		if file_rateUs then
